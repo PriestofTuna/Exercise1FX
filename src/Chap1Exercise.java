af8b25a7aee344fa6498b16de7a467bea06da121
PriestofTuna <lyttekk@yahoo.com>
2016-07-02 23:58:00 -0400

    Adding exercise 1.9 (Chapter 1)FX

--- src/Chap1Exercise.java
+++ src/Chap1Exercise.java
@@ -1,21 +1,53 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import static javafx.application.Application.launch;
+import static javafx.geometry.Pos.CENTER;
+
 /**
  * Created by lytte on 6/27/2016.
  *
  * Purpose, rectangle area
  */
-public class Chap1Exercise {
+public class Chap1Exercise extends Application {
     public static void main(String [] args) {
-        //taking 1.9
+    launch(args);
+    }
+    Scene scene;
+    Button button = new Button("rectangle 4.5:7.9");
+    Text output = new Text("Output ");
+    Label areaofRectangle = new Label("Click button for Rectangle area");
+    public void start(Stage primaryStage) throws Exception {
         double width, height;
         width = 4.5;
         height = 7.9;
-        area(width, height);
+        button.setOnAction(e -> {
+        output.setText("" + area(width, height));
+        });
+        GridPane gridA = new GridPane();
+        gridA.setAlignment(CENTER);
+        gridA.setVgap(15.0);
+        gridA.setHgap(10.0);
+        gridA.add(areaofRectangle, 0, 0);
+        gridA.add(button, 1, 0);
+        gridA.add(output, 0, 1);
+        scene = new Scene(gridA, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+        //taking 1.9
+
+
         //creates a method for finding the area of a rectangle
         public static double area(double w, double h) {
 
         double print=w*h;
-        System.out.println("The area is " +print);
         return print;
     }
 
